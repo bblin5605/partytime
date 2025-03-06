646f41d203936a1883700165f49b11661e1537c5
bblin5605 <bblin5605@gmail.com>
2025-03-06 14:15:23 +0800

    fix: 將 BrowserRouter 改為 HashRouter 以支援 GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CompareGame from './pages/CompareGame';
 import BombGame from './pages/BombGame';
